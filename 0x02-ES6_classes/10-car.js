fded30e81ed6be87f5c0168104dfac9874fc5bad
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-06-28 19:35:47 +0300

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,17 +1,17 @@
 const mySymbol = Symbol('cloneCar');
 
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    [mySymbol]() {
-        return new this.constructor()
-    }
+  [mySymbol]() {
+    return new this.constructor();
+  }
 
-    cloneCar() {
-        return this[mySymbol]();
-    }
+  cloneCar() {
+    return this[mySymbol]();
+  }
 }
