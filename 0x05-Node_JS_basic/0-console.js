266666191705201c58ac4bde8839d0d5fd27e4f3
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-19 23:51:57 +0300

    task 1

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,7 @@
  * Displays a message on the STDOUT.
  * @param {String} message The message to display.
  */
-function displayMessage(message) {
-  process.stdout.write(`${message}\n`);
-}
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 module.exports = displayMessage;
