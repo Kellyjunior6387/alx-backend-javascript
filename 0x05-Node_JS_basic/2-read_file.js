f9f9587f4bfbeb87b952445edb61afa2635e4a3f
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-20 11:22:56 +0300

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,28 +5,26 @@
 const fs = require('fs');
 
 function countStudents(file) {
-    if (!fs.existsSync(file)) {
-        throw new Error('Cannot load the database');
-    }
-    if (!fs.statSync(file).isFile()) {
-        throw new Error('Cannot load the database');
-    }
+  try {
     const data = fs.readFileSync(file, 'utf-8').trim().split('\n');
     data.splice(0, 1);
     let totalStudents = 0;
     const subjects = {};
 
     data.forEach((element) => {
-        const line = element.split(',');
-        if (!subjects[line[3]]) {
-            subjects[line[3]] = [];
-        }
-        subjects[line[3]].push(line[1]);
-        totalStudents += 1;
+      const line = element.split(',');
+      if (!subjects[line[3]]) {
+        subjects[line[3]] = [];
+      }
+      subjects[line[3]].push(line[1]);
+      totalStudents += 1;
     });
     console.log(`Number of students: ${totalStudents}`);
     for (const [fields, students] of Object.entries(subjects)) {
-        console.log(`Number of students in ${fields}: ${students.length}. List: ${students.join(', ')}`);
+      console.log(`Number of students in ${fields}: ${students.length}. List: ${students.join(', ')}`);
     }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
