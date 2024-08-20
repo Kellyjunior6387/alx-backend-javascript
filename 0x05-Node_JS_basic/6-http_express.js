cc5accf26aa9493afb869af41d28ab1f13b19ae6
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-20 12:31:15 +0300

    task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+module.exports = app;
