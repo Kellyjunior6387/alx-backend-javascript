938761fd560aabc4edc7a54d1871c7be479b849d
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-20 11:11:09 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 /*
 A script that takes input from the command line and prints it
 */
-process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name.trim()}\n`);
-    process.stdin.end();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
