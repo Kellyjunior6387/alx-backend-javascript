fe39c27f13b6a16b87c1291c75423fd1f4f23e25
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-20 11:36:20 +0300

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,24 +9,26 @@ const countStudents = (file) => new Promise((resolve, reject) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     }
-    const cleanedData = data.trim().split('\n');
-    cleanedData.splice(0, 1);
-    let totalStudents = 0;
-    const subjects = {};
+    if (data) {
+      const cleanedData = data.trim().split('\n');
+      cleanedData.splice(0, 1);
+      let totalStudents = 0;
+      const subjects = {};
 
-    cleanedData.forEach((element) => {
-      const line = element.split(',');
-      if (!subjects[line[3]]) {
-        subjects[line[3]] = [];
+      cleanedData.forEach((element) => {
+        const line = element.split(',');
+        if (!subjects[line[3]]) {
+          subjects[line[3]] = [];
+        }
+        subjects[line[3]].push(line[1]);
+        totalStudents += 1;
+      });
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [fields, students] of Object.entries(subjects)) {
+        console.log(`Number of students in ${fields}: ${students.length}. List: ${students.join(', ')}`);
       }
-      subjects[line[3]].push(line[1]);
-      totalStudents += 1;
-    });
-    console.log(`Number of students: ${totalStudents}`);
-    for (const [fields, students] of Object.entries(subjects)) {
-      console.log(`Number of students in ${fields}: ${students.length}. List: ${students.join(', ')}`);
+      resolve(true);
     }
-    resolve(true);
   });
 });
 module.exports = countStudents;
