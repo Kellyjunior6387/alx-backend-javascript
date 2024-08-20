c21c211f44a56da228a1e538652ebbb29daf85f3
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-20 11:08:32 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,10 +5,13 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-    const name = process.stdin.read()
-    process.stdout.write(`Your name is: ${name}\n`);
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name.trim()}\n`);
+    process.stdin.end();
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
