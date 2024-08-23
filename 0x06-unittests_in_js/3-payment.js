f6e6643c0d7898588a36a750596ea1400a0c8bcd
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-23 13:00:22 +0300

    changing the log message format

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,6 +2,6 @@ const Utils = require('./utils')
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
     result = Utils.calculateNumber('SUM', totalAmount, totalShipping)
-    console.log(`The total sum is ${result}`);
+    console.log(`The total is: ${result}`);
 }
 module.exports = sendPaymentRequestToApi
